c7bc9872543a0fd0c70221b6e343573438eaaf7c
Nam Anh Tran <steve200994@gmail.com>
2025-10-14 17:23:54 +1100

    temporary critque changes

---
+++ scripts.js
@@ -0,0 +1,40 @@
+//for the sake of readability, 
+// I am going to use snake_case instead of camelCase (i code more in python haha)
+
+//---variable holders---//
+let map = 'graphs/map.json';
+let boxplot = 'graphs/boxplot.json';
+let line_chart = 'graphs/line_chart.json'
+let area_chart = "graphs/area_chart.json"
+let heatmap = 'graphs/heatmap.json'
+
+//1. Map
+vegaEmbed(".map", map).then(function(res){
+    console.log(res);
+}).catch(console.error)
+
+//2. boxplot
+vegaEmbed(".boxplot", boxplot).then(function(res){
+    console.log(res);
+}).catch(console.error)
+
+//3. Line Chart
+vegaEmbed(".line_chart", line_chart).then(function(res){
+    console.log(res);
+}).catch(console.error)
+
+
+//4. Heat Map
+vegaEmbed('.heatmap', heatmap).then(function(res){
+    console.log(res);
+}).catch(console.error)
+
+//5. area_chart
+vegaEmbed(".area_chart", area_chart).then(function(res){
+    console.log(res);
+}).catch(console.error)
+
+
+
+
+
